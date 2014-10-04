fc0bbffe0e6a7e36a6fc7759be3c5eeaf8594cff
alexbokii <vanilinka33@gmail.com>
2014-10-04 14:07:28 +0100

    Added functionality to calculate number which raised to the power 2 and the square root of number)

--- script.js
+++ script.js
@@ -1,39 +1,61 @@
 $(function() {
   var firstOperand = 0;
   var secondOperand = 0;
-  var operator;
+  var operator = undefined;
+  var operatorsArray = [];
 
   $('#numbers li').on('click', function() {
-    if (firstOperand === 0 && operator === undefined) {
-      firstOperand = $(this).text();
-      $('.workplace').text(firstOperand);
-    }
-    else if (firstOperand != 0 && operator === undefined) {
-      firstOperand += $(this).text();
-      $('.workplace').text(firstOperand);
-    }
-    else if(firstOperand != 0 && operator != undefined && secondOperand === 0) {
-      secondOperand = $(this).text();
-      $('.workplace').text(secondOperand);
-    }
-    else if(firstOperand != 0 && operator != undefined && secondOperand != 0) {
-      secondOperand += $(this).text();
-      $('.workplace').text(secondOperand);
-    }
-    console.log(firstOperand, secondOperand);
+    setOperand($(this));
   });
 
   $("#operators li").on('click', function() {
-    var operatorSign = $(this).text();
-    operator = operatorSign;
-    $('.workplace').text(operator);
-    console.log(operator);
+    setOpeartor($(this));
   });
 
   $("#count").on('click', calculate);
 
+  $("#clear-all").on('click', clearAll);
+
+  function setOperand(el) {
+    if (firstOperand === 0 && operator === undefined) {
+      firstOperand = el.text();
+      $('.workplace').text(firstOperand);
+    }
+    else if (firstOperand != 0 && operator === undefined) {
+      firstOperand += el.text();
+      $('.workplace').text(firstOperand);
+    }
+    else if(firstOperand != 0 && operator != undefined && secondOperand === 0) {
+      secondOperand = el.text();
+      $('.workplace').text(secondOperand);
+    }
+    else if(firstOperand != 0 && operator != undefined && secondOperand != 0) {
+      secondOperand += el.text();
+      $('.workplace').text(secondOperand);
+    }
+    console.log(firstOperand, secondOperand);
+  }
+
+  function setOpeartor(el) {
+    var operatorSign = el.text();
+    operator = operatorSign;
+    if(operator === 'x2') {
+      calculate();
+    }
+    else if (operator === '') {
+      calculate();
+    }
+    else if (el.hasClass('sq-root') == true) {
+      operator = "sq-r";
+      calculate();
+    }
+    operatorsArray.push(operator);
+    if(operatorsArray.length > 1) {
+      calculate();
+    }
+  }
+
   function calculate() {
-    console.log("I'm in calculate");
     var result;
     if(operator === "+") {
       result = parseInt(firstOperand) + parseInt(secondOperand);
@@ -47,7 +69,38 @@ $(function() {
     else if (operator === "/") {
       result = parseInt(firstOperand) / parseInt(secondOperand);
     }
+    else if (operator === "x2") {
+      result = parseInt(firstOperand) * parseInt(firstOperand);
+    }
+    else if (operator === "&radic;") {
+      result = parseInt(firstOperand) / parseInt(secondOperand);
+    }
+    else if (operator === "sq-r") {
+      var sqRoot = parseInt(firstOperand); 
+      for(var i = 2; i < 100; i++) {
+        if (i * i === sqRoot) {
+          result = i;
+        }
+        // else {
+        //   console.log("Sotty, number " + sqRoot + " dosn't have multiplier");
+        // }
+      }
+    }
     $('.workplace').text(result);
-    console.log(result);
+    makeResultFirstOperand(result);
+  }
+
+  function makeResultFirstOperand(num) {
+    firstOperand = num;
+    secondOperand = 0;
+    operator = 0;
+  } 
+
+  function clearAll() {
+    operatorsArray = [];
+    firstOperand = 0;
+    secondOperand = 0;
+    operator = undefined;
+    $('.workplace').text("0");
   }
 });
